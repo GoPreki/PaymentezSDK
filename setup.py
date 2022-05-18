486363f728edce45943569cddbc33d3f190e62a3
Kevin <kevin@preki.com>
2022-05-18 11:58:22 -0500

    chore[setup]: Initial setup for the Paymentez SDK

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='paymentez',
+    version='1.0.0',
+    author='Preki',
+    author_email='kevin@preki.com',
+    packages=['paymentez', 'paymentez.models', 'paymentez.utils'],
+    url='https://preki.com',
+    download_url='https://github.com/GoPreki/PaymentezSDK',
+    license='MIT',
+    description='Python library for handling Paymentez integration',
+    long_description='Python library for handling Paymentez integration',
+    install_requires=[
+        'requests==2.27.0',
+    ],
+)
