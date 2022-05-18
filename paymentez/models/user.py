26427a5191b6998188f3033e1a5c1fac15a4d1a7
Kevin <kevin@preki.com>
2022-05-18 17:11:20 -0500

    feat[user]: added user models
    body: there are different information of users in differents services

---
+++ paymentez/models/user.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class TypeUser(Enum):
+    NATURAL = 'N'
+    LEGAL = 'J'
+
+class TypeFiscalNumber(Enum):
+  CC = 'CC'
+  CE = 'CE'
+  NIT = 'NIT'
+  TI = 'TI'
+  PP = 'PP'
+  IDC = 'IDC'
+  CEL = 'CEL'
+  RC = 'RC'
+  DE = 'DE'
+
+@dataclass
+class SimpleUser:
+    id: str
+    email: str
+    name: str
+
+    @staticmethod
+    def from_dict(res: dict) -> 'SimpleUser':
+        return SimpleUser(
+            id=res['id'],
+            email=res['email'],
+            name=res['name'],
+        )
+
+@dataclass
+class User(SimpleUser):
+    fiscal_number: str
+    type: TypeUser
+    type_fis_number: TypeFiscalNumber
+    ip_address: str
